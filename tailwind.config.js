f69672cdad4ff268321001c6ad4cef8020ff8d60
ghada <ghadaemad011@gmail.com>
2023-07-23 22:49:08 +0300

    task5

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "Moderate_violet": "hsl(263, 55%, 52%)",
+        "Very_dark_grayish_blue": "hsl(217, 19%, 35%)",
+        "Very_dark_blackish_blue": "hsl(219, 29%, 14%)",
+        "White": "hsl(0, 0%, 100%)",
+        "Light_gray": "hsl(0, 0%, 81%)",
+        "Light_grayish_blue": "hsl(210, 46%, 95%)",
+      },
+      
+      fontFamily:{
+        'Barlow': ['Barlow Semi Condensed', 'sans-serif']
+
+      }
+    },
+  },
+  plugins: [],
+}
+
